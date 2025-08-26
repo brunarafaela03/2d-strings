9ec13b5d85f04c6f106f2619221f04afd60e9bb9
brunarafaela03 <bruna.pereira03@escola.pr.gov.br>
2025-08-26 14:58:54 +0000

    aula 6

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
@@ -6,7 +6,39 @@ function mostraPalavrasChave() {
 
   const campoResultado = document.querySelector("#resultado-palavrachave");
 
-  const palavras = texto.split(" ");
+  const palavrasChave = processaTexto(texto);
 
-  campoResultado.textContent = palavras.join(",");
+  campoResultado.textContent = palavrasChave.join(",");
+
+}
+
+
+function contaFrequencias(palavras)
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+  const frequencias = contaFrequencias(palavras);
+  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+  function ordenaPalavra(p1, p2) {
+  return frequencias[p2] - frequencias[p1];
+}
+  console.log(ordenadas);
+  return ordenadas.slice(0, 10);
+}
+
+
+  let frequencias = {};
+
+  for (let i of palavras) {
+    frequencias[i] = 0;
+
+    for (let j of palavras) {
+      if (i == j) {
+        frequencias[i]++;
+      }
+    }
+  }
+
+  //console.log(frequencias);
+
+  return frequencias;
 }
