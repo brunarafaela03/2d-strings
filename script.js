2082ea7e442fbefdfc4f49d9b7662c00d8ac6ec9
brunarafaela03 <bruna.pereira03@escola.pr.gov.br>
2025-09-16 11:47:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,55 @@
+import { PALAVRAS_RUINS } from "./palavrasRuins.js";
+
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-  const texto = document.querySelector("#entrada-de-texto").value;
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave = processaTexto(texto);
 
-  const campoResultado = document.querySelector("#resultado-palavrachave");
-
-  const palavrasChave = processaTexto(texto);
-
-  campoResultado.textContent = palavrasChave.join(",");
-
+    campoResultado.textContent = palavrasChave.join(", ");
 }
 
-
-function contaFrequencias(palavras)
 function processaTexto(texto) {
-  let palavras = texto.split(/\P{L}+/u);
-  const frequencias = contaFrequencias(palavras);
-  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
-  function ordenaPalavra(p1, p2) {
-  return frequencias[p2] - frequencias[p1];
-}
-  console.log(ordenadas);
-  return ordenadas.slice(0, 10);
-}
+    let palavras = texto.split(/\P{L}+/u);
 
-
-  let frequencias = {};
-
-  for (let i of palavras) {
-    frequencias[i] = 0;
-
-    for (let j of palavras) {
-      if (i == j) {
-        frequencias[i]++;
-      }
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
     }
-  }
 
-  //console.log(frequencias);
+    palavras = tiraPalavrasRuins(palavras);
 
-  return frequencias;
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
+    let frequencias = {};
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+            }
+        }
+    }
+    return frequencias;
+}
+
+function tiraPalavrasRuins(palavras) {
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
